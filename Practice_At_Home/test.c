f0ffcb9c7095e006b6ca27eaeaa791dc198da0ed
RockyHandsome8 <101408698+RockyHandsome8@users.noreply.github.com>
2024-10-18 22:45:22 +0530

    sdgr

--- Practice_At_Home/test.c
+++ Practice_At_Home/test.c
@@ -2,5 +2,16 @@
     
 void main()
 {
-    
+    // fibonacci series 
+    // 1. using recursive functions
+    // 2. using loops (all of them)
+
+    // Sum of the digits of a 4 digit number
+    // Check if a number is prime or not.
+
+
+
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
 }
