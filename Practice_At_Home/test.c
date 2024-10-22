e1a0093f9188cef2bc51eb15585d04b281d711bc
RockyHandsome8 <101408698+RockyHandsome8@users.noreply.github.com>
2024-10-22 21:08:01 +0530

    sdhg8\

--- Practice_At_Home/test.c
+++ Practice_At_Home/test.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 
-int fib(int n)
-{
-    if (n == 0)
-    {
-        return 0;
-    }
-    else if (n == 1)
-    {
-        return 1;
-    }
+// // Recursive function to calculate Fibonacci series
+// int fibonacci(int n)
+// {
+//     if (n <= 1)
+//         return n;
+//     else
+//         return fibonacci(n - 1) + fibonacci(n - 2);
+// }
 
-    // return fib(n - 1) + fib(n - 2);
-    return fib(n - 1);
-}
+// // Function to print the Fibonacci series up to the nth number
+// void print_fibonacci_series(int num)
+// {
+//     for (int i = 0; i < num; i++)
+//     {
+//         printf("%d ", fibonacci(i));
+//     }
+// }
 
 void main()
 {
@@ -24,9 +27,41 @@ void main()
     // Sum of the digits of a 4 digit number
     // Check if a number is prime or not.
 
-    int num;
+    // int n;
+
+    // // Input the number of terms for the Fibonacci series
+    // printf("Enter the number of terms: ");
+    // scanf("%d", &n);
+
+    // if (n <= 0)
+    // {
+    //     printf("Please enter a positive integer.\n");
+    // }
+    // else
+    // {
+    //     printf("Fibonacci Series:\n");
+    //     print_fibonacci_series(n);
+    // }
+
+    int t1 = 0, t2 = 1, nextTerm, num, i = 1;
+
     printf("Enter a number: ");
     scanf("%d", &num);
 
-    printf("%d", fib(num));
+    // for (int i = 1; i <= num; i++) {
+    //     printf("%d ", t1);
+    //     nextTerm = t1 + t2;  // Calculate the next term
+    //     t1 = t2;             // Update t1 to the next term
+    //     t2 = nextTerm;       // Update t2 to the next term
+    // }
+
+    do
+    {
+        printf("%d ", t1);
+        nextTerm = t1 + t2; // Calculate the next term
+        t1 = t2;            // Update t1 to the next term
+        t2 = nextTerm;      // Update t2 to the next term
+
+        i++;
+    } while (i <= num);
 }
